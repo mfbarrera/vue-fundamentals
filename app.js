0a51964c6c14e5d06346d0f98e8a84ecc68e2370
Sebastian Benitez Ospina <jsb1439@hotmail.com>
2020-09-13 20:28:10 -0500

    Components with Props

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+Vue.component('plan', {
+	template: '#plan-template',
+	props: {
+		name: {
+			type: String,
+			required: true
+		}
+	}
+})
+
 new Vue({
-	el: '#app'
+	el: '#app',
+	data: {
+		plans: ['The Single', 'The Curious', 'The Addict']
+	}
 });
